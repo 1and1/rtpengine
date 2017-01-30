71b1dd589a14effff543e91da5d8a45cc9e44655
Lucian Balaceanu <lucian.balaceanu@gmail.com>
2017-01-30 15:35:44 +0100

    Adding a simple queue implementation

---
+++ daemon/simple_queue.c
@@ -0,0 +1,60 @@
+#include "simple_queue.h"
+
+
+inline int qAlloc(SQueue *q, int size) {
+	if (!q)
+		return -1;
+
+	q->intArray = (int*)malloc(size * sizeof(int));
+	q->front = 0;
+	q->rear = -1;
+	q->itemCount = 0;
+	q->fullSize = size;
+}
+
+inline  int qFree(SQueue *q, int size) {
+	if (!q)
+		return -1;
+
+	free(q->intArray);
+}
+
+inline int peek(SQueue *q) {
+   return q->intArray[front];
+}
+
+inline int isEmpty(SQueue *q) {
+   return q->itemCount == 0;
+}
+
+inline int isFull(SQueue *q) {
+   return q->itemCount == q->fullSize;
+}
+
+inline int size(SQueue *q) {
+   return q->itemCount;
+}
+
+inline void insert(SQueue *q, int data) {
+
+   if(!isFull()) {
+
+      if(q->rear == q->fullSize-1) {
+         q->rear = -1;
+      }
+
+      q->intArray[++q->rear] = data;
+      q->itemCount++;
+   }
+}
+
+inline int removeData(SQueue *q) {
+   int data = intArray[q->front++];
+
+   if(q->front == q->fullSize) {
+      q->front = 0;
+   }
+
+   q->itemCount--;
+   return data;
+}
