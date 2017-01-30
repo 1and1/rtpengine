aa37ab6205e4bdb13de54804cd5b68079c585e4f
Lucian Balaceanu <lucian.balaceanu@gmail.com>
2017-01-30 15:35:44 +0100

    Adding a simple queue implementation

---
+++ daemon/simple_queue.h
@@ -0,0 +1,27 @@
+#ifndef _SIMPLE_QUEUE_H_
+#define _SIMPLE_QUEUE_H_
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+typedef struct SQueue {
+	int *intAray;
+	int fullSize;
+	int front;
+	int rear;
+	int itemCount;
+} SQueue;
+
+int qAlloc(SQueue *q, int size);
+int qFree(SQueue *q, int size);
+int peek(SQueue *q);
+int isEmpty(SQueue *q);
+int isFull(SQueue *q);
+int size(SQueue *q);
+void insert(SQueue *q, int data) ;
+int removeData(SQueue *q) ;
+
+#endif
